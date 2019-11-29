3a7b85cab265f8d2426a834f4f4cc15af87b82e5
unknown <chap6680@gmail.com>
2019-11-29 15:04:22 -0700

    added controllers - also changed name of routes oging forward

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const placesRoutes = require('./routes/places-route');
 
+const placesRoutes = require('./routes/places-routes');
 
-const app=express();
+const app = express();
 
-app.use('/api/places', placesRoutes);
+app.use('/api/places', placesRoutes); // => /api/places...
 
-app.use((error,req,res,next)=>{
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code||500);
-    res.json({message:error.message || "unknwown error"});
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
 });
 
 app.listen(5000);
